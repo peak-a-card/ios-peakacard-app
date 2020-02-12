ea8102a466d5f080edf5a3f7a7fb250b83e5cc0a
Miquel Alvarado <miquel.alvarado@scmspain.com>
2020-02-12 19:13:19 +0100

    Initial Commit

---
+++ Peak a CardTests/Peak_a_CardTests.swift
@@ -0,0 +1,34 @@
+//
+//  Peak_a_CardTests.swift
+//  Peak a CardTests
+//
+//  Created by Miquel Alvarado on 12/02/2020.
+//  Copyright © 2020 peakacard. All rights reserved.
+//
+
+import XCTest
+@testable import Peak_a_Card
+
+class Peak_a_CardTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
