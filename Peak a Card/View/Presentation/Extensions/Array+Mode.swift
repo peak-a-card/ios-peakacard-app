9515446f8370f186d79261b57d26752fad550888
Miquel Alvarado <miquel.alvarado@scmspain.com>
2020-04-26 09:10:28 +0200

    Remove print

--- Peak a Card/View/Presentation/Extensions/Array+Mode.swift
+++ Peak a Card/View/Presentation/Extensions/Array+Mode.swift
@@ -6,7 +6,6 @@ extension Array where Element == Float {
         let grouped = Dictionary(grouping: self, by: { $0 })
         if let maxGroup = grouped.max(by: { $0.value.count < $1.value.count }) {
             let maxGroups = grouped.filter { $0.value.count == maxGroup.value.count }
-            print(maxGroups.map { $0.key })
             return maxGroups.map { $0.key }
         }
 
