669a37e9dfe2e8a1b340ecefe148e785b0c9444d
Miquel Alvarado <miquel.alvarado@scmspain.com>
2020-05-08 16:46:43 +0200

    Provide a button to manually submit card

--- Peak a Card/View/Features/Card Detail/CardDetailView.swift
+++ Peak a Card/View/Features/Card Detail/CardDetailView.swift
@@ -33,24 +33,44 @@ struct CardDetailView: View {
                     })
 
                 if self.store.state.selectedCard != nil {
-                    CardView(card: self.store.state.selectedCard!, isFlipped: .constant(true))
-                        .frame(width: 200, height: 200 * 1.4, alignment: .center)
-                        .offset(y: self.selectedCardOffset.height)
-                        .animation(.interactiveSpring())
-                        .gesture(DragGesture()
-                            .onChanged { self.selectedCardOffset = $0.translation }
-                            .onEnded { value in
-                                let result = self.getInteractionResult(geometry, from: value)
-                                switch result {
-                                case .submit:
-                                    self.store.dispatch(action: .detail(.submit))
-                                case .resetPosition:
-                                    self.selectedCardOffset = .zero
-                                case .dismiss:
-                                    self.store.dispatch(action: .detail(.dismiss))
+                    VStack(alignment: .center) {
+                        CardView(card: self.store.state.selectedCard!, isFlipped: .constant(true))
+                            .frame(width: 200, height: 200 * 1.4, alignment: .center)
+                            .offset(y: self.selectedCardOffset.height)
+                            .animation(.interactiveSpring())
+                            .gesture(DragGesture()
+                                .onChanged { self.selectedCardOffset = $0.translation }
+                                .onEnded { value in
+                                    let result = self.getInteractionResult(geometry, from: value)
+                                    switch result {
+                                    case .submit:
+                                        self.store.dispatch(action: .detail(.submit))
+                                    case .resetPosition:
+                                        self.selectedCardOffset = .zero
+                                    case .dismiss:
+                                        self.store.dispatch(action: .detail(.dismiss))
+                                    }
                                 }
+                        )
+
+                        Button(action: {
+                            self.selectedCardOffset = CGSize(width: 0, height: -geometry.size.height)
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                self.store.dispatch(action: .detail(.submit))
                             }
-                    )
+                        }) {
+                            Text("card_detail_submit")
+                                .foregroundColor(Stylesheet.color(.primary))
+                                .font(Stylesheet.font(.m))
+                        }
+                        .padding()
+                        .background(Stylesheet.color(.background))
+                        .cornerRadius(4)
+                        .overlay(RoundedRectangle(cornerRadius: 4)
+                        .stroke(Stylesheet.color(.primary)))
+                        .padding(.top)
+                        .opacity(self.selectedCardOffset == .zero ? 1.0 : 0.0)
+                    }
                 }
             }
         }
